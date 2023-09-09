44f83cbff635e7a0150ccddbfc5cfaf2c670effe
Ryodai Matsui <rmatsui@sas.upenn.edu>
2023-09-09 17:12:55 -0400

    third commit

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -38,6 +38,7 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
+# Main page of the API that shows the different routes available
 @app.route("/")
 def welcome():
     return(
@@ -53,54 +54,76 @@ def welcome():
 # Precipitation route
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    # Calculate the date one year from 2017-08-23
     one_year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
     
+    # Query to access the date and precipitation for the past year
     prcp_data = session.query(Measurement.date, Measurement.prcp).\
     filter(Measurement.date >= one_year_ago).order_by(Measurement.date).all()
 
+    # List comprehension to create a key and value pair 
     prcp_dict = [{"date": date, "percipitation": prcp} for date, prcp in prcp_data]
     
+    # Output as JSON 
     return jsonify(list(np.ravel(prcp_dict)))
 
 # Stations route
 @app.route("/api/v1.0/stations")
 def stations():
+    # Query for all stations
     results= session.query(Station.station).all()
-    results_stations = list(np.ravel(results))
-    
-    return jsonify(results_stations)
+    # Output as JSON
+    return jsonify(list(np.ravel(results)))
 
-# Tobs route
+# Tobs route 
 @app.route("/api/v1.0/tobs")
 def tobs():
+    # Calculating the date one year from 2017-08-23
     one_year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
-
+    
+    # Query for date and tobs value based on the most active station 
     active_station = session.query(Measurement.date, Measurement.tobs).\
     filter(Measurement.station == 'USC00519281').\
     filter(Measurement.date >= one_year_ago).all()
 
+    # List comprehension to create a key and value pair 
     most_active_station = [{"date": date, "temp": temp} for date, temp in active_station]
+    
+    # Output as JSON
     return jsonify(list(np.ravel(most_active_station)))
+
 
 # Dynamic route for a given start date
 @app.route("/api/v1.0/<start>")
 def start_date_temps(start=None):
+    # Place query elements in variable 'sel'
     sel = [Measurement.date, func.min(Measurement.tobs),func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    
+    # Query the elements and filter according to start_date input by user
     results = session.query(*sel).filter(Measurement.date >= start).group_by(Measurement.date).all()
     
+    # List comprehension to create key-value pairs
     temps = [{"date": date, "min_temp": min_temp, "avg_temp": avg_temp, "max_temp": max_temp}\
             for date, min_temp, avg_temp, max_temp in results]
     
+    # Output as JSON
     return jsonify(list(np.ravel(temps)))
 
 # Dynamic route for a given start and end date
 @app.route("/api/v1.0/<start>/<end>")
 def date_range_temps(start=None, end=None):
+    # Place query elements in variable 'sel'
     sel = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    
+    # Query the elements and filter according to start and end date input by user
     results = session.query(*sel).filter(Measurement.date >= start).\
             filter(Measurement.date <= end).group_by(Measurement.date).all()
+    
+    # List comprehension to create key-value pairs
     temps = [{"date": date, "min_temp": min_temp, "avg_temp": avg_temp, "max_temp": max_temp}\
              for date, min_temp, avg_temp, max_temp in results]
+    
+    # Output as JSON
     return jsonify(list(np.ravel(temps)))
 
 if __name__ == '__main__':
